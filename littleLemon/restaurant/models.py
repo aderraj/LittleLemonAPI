e442c980d933cadfb0dcac38b17f32c9e3213596
aderraj <none>
2025-10-10 03:30:37 +0100

    init models

--- littleLemon/restaurant/models.py
+++ littleLemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    number_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return f"Booking for {self.name} on {self.BookingDate}"
